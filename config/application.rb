4b319e0951493c20c226afc7c6a329d4be5140fa
Paulo Rodrigues <paulor2222@gmail.com>
2020-10-31 21:06:44 -0300

    configured test env

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,13 @@ module Twiiter
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        controller_specs: false,
+        request_specs: false,
+        routing_specs: false,
+        helper_specs: false
+    end
   end
 end
