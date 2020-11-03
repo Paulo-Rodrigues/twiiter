412ea99191f0cd778a909f23e9d060c5bf4d42b1
Paulo Rodrigues <paulor2222@gmail.com>
2020-11-03 12:09:14 -0300

    Like model validations

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,10 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :likeable, polymorphic: true
 
-  validates :user_id, presence: true
-  validates :likeable_id, presence: true
-  validates :likeable_type, presence: true
+  validates :user_id, presence: true,
+                      uniqueness: { scope: %i[likeable_id likeable_type], message: 'Já curtido' }
+  validates :likeable_id, presence: true,
+                          uniqueness: { scope: %i[user_id likeable_type], message: 'Já curtido' }
+  validates :likeable_type, presence: true,
+                            uniqueness: { scope: %i[user_id likeable_id], message: 'Já curtido' }
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -10,5 +10,25 @@ RSpec.describe Like, type: :model do
     it { is_expected.to validate_presence_of(:user_id) }
     it { is_expected.to validate_presence_of(:likeable_id) }
     it { is_expected.to validate_presence_of(:likeable_type) }
+
+    it 'valid like like just one time' do
+      user = create(:user)
+      twiit = create(:twiit)
+      like = Like.create(user: user, likeable: twiit)
+
+      expect(like).to be_valid
+      expect(Like.count).to eq(1)
+      expect(user.likes.count).to eq(1)
+      expect(twiit.likes.count).to eq(1)
+    end
+
+    it 'invalid like, trying more than one like at same twiit' do
+      user = create(:user)
+      twiit = create(:twiit)
+      Like.create(user: user, likeable: twiit)
+      other_like = Like.new(user: user, likeable: twiit)
+
+      expect(other_like).not_to be_valid
+    end
   end
 end
